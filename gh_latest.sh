dc7f7635b0351a73a650688c0db4a4217320d42d
John Pappas <163186936+pappasjfed@users.noreply.github.com>
2024-06-19 21:41:59 -0600

    Add gh script to download ALL the release assets for media creation

---
+++ gh_latest.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+SRC=$(basename "$PWD")
+REPO="JacobsFederal/$SRC"
+DEST="$HOME/Downloads/$SRC"
+
+echo -e "\nWorking with github repo $REPO"
+echo -e "Listing releases\n"
+gh release list -R $REPO
+echo -e "\nDownloading Latest to $DEST\n"
+gh release download -p "*" -D $DEST -R $REPO
+ls -Alht $DEST 
+if [ $? ];then
+  echo -e "\nDownload Complete!"
+else
+  echo "ERROR!"
+fi
